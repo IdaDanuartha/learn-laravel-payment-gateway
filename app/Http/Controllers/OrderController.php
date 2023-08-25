1303fe46b1cabc02f3dacc91b201538227cb544f
IdaDanuartha <danuart21@gmail.com>
2023-08-25 11:14:57 +0800

    little update in controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -50,7 +50,7 @@ class OrderController extends Controller
         $hashed = hash('sha512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
 
         if($hashed == $request->signature_key) {
-            if($request->transaction_status == 'capture' or $request->transaction_status == 'settlement') {
+            if($request->transaction_status == 'capture' || $request->transaction_status == 'settlement') {
                 $order = Order::find($request->order_id);
                 $order->update(['status' => 'Paid']);
             }

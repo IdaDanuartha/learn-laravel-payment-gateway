9032fdea5a4e73f4e1e8f99af062e699b5271883
IdaDanuartha <danuart21@gmail.com>
2023-08-28 13:34:57 +0800

    little update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,14 +14,14 @@ class OrderController extends Controller
 
     public function checkout(Request $request)
     {
-        $request->request->add(['total_price' => $request->qty * 100000]);
+        $request->request->add(['total_price' => $request->qty * 1000]);
         
         $order = Order::create($request->all());
 
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = config('midtrans.server_key');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isProduction = config('midtrans.is_production');
         // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
